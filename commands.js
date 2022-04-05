c0a0c40a7f91060ac7fa071f3660a724d1db381b
Jesse-Bett <jessekip21@gmail.com>
2022-04-06 02:27:58 +0300

    New file for handling command methods
    This file will handle methods implementing the ALLOCATE, SIP, CHANGE, BALANCE & REBALANCE commands.

---
+++ commands.js
@@ -0,0 +1,25 @@
+
+// Coding the input commands as functions.
+
+let ALLOCATE = function(){
+
+};
+
+let SIP = function (){
+
+};
+
+let CHANGE = function() {
+    
+};
+
+
+let BALANCE =  function() {
+    
+};
+
+let REBALANCE = function(){
+
+};
+
+
