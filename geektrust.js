af47e593d8fe58adeb093e30089489a9c595fdcb
Jesse-Bett <jessekip21@gmail.com>
2022-04-05 14:37:51 +0300

    Setting up algo as comments
    Comming up with the algorithm to find the solution and setting it up in the main file as comments

--- geektrust.js
+++ geektrust.js
@@ -1,1 +1,43 @@
+// Declaring dependencies.
+
+
+
+
+// Getting input from textfile , taking the location to the textfile as a parameter.
 const filename = process.argv[2];
+
+
+
+
+
+
+
+// Coding the input commands as functions.
+
+let ALLOCATE = function(){
+
+};
+
+let SIP = function (){
+
+};
+
+let CHANGE = function() {
+    
+};
+
+
+let BALANCE =  function() {
+    
+};
+
+let REBALANCE = function(){
+
+};
+
+
+
+
+
+
+// Printing the output to the console.
