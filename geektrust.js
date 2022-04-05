5329a803b0c869d2a8cda6902f137b03034d26ee
Jesse-Bett <jessekip21@gmail.com>
2022-04-06 02:24:08 +0300

    Implemeted line by line,word by word file parsing

--- geektrust.js
+++ geektrust.js
@@ -1,45 +1,42 @@
 // Declaring dependencies.
 const fs = require('fs');
+const commands = require('./commands')
 
 
+let monthCount = 0
+let moneyObject
 
 // Getting input from textfile , taking the location to the textfile as a parameter.
 const inputFile = process.argv[2];
 
-fs.readFile(inputFile, 'utf8', function(err, data) {  //using fs module to read commands from input file
-    if (err) throw err;
-    console.log(data);
-});
+if (!inputFile) { // Base case where there was no input file.
+    console.log("No input file detected")
+  }
+
+let reader = new FileReader();
+  reader.onload = function(){
+
+    // going through the file line by line
+    let lines = this.result.split('\n');
+    for(let line = 0; line < lines.length; line++){
+      inputLine = line.split("")  //creating an array of single words of each line
+
+      if (inputLine[0] == 'ALLOCATE'){}
+                 
+      else if (inputLine[0] == 'SIP'){}
+                  
+      else if (inputLine[0] == 'CHANGE'){}
+
+      else if (inputLine[0] == 'BALANCE'){}
+                       
+      else if (inputLine[0] == 'REBALANCE'){}
+            
+    }
+  };
+  reader.readAsText(inputFile);
 
 
 
-
-
-
-
-
-// Coding the input commands as functions.
-
-let ALLOCATE = function(){
-
-};
-
-let SIP = function (){
-
-};
-
-let CHANGE = function() {
-    
-};
-
-
-let BALANCE =  function() {
-    
-};
-
-let REBALANCE = function(){
-
-};
 
 
 
